e646f08febd25b10671ffd4a612eecba671e1124
Ayush <ayushnigam072@gmail.com>
2025-01-17 14:04:50 +0530

    Added division

---
+++ division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int k=a/b;
+        System.out.println(k/c);
+    }
+}
